8b95f4088725ab1dc3b31ccc30272f53c0bc6d33
GrayCoded <130934177+GrayCoded@users.noreply.github.com>
2023-07-18 18:21:35 -0300

    successfull test completed

--- index.js
+++ index.js
@@ -1,81 +1,80 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const {Shape, Triangle, Circle, Square} = require("./lib/shapes.js");
+const { Shape, Triangle, Circle, Square } = require("./lib/shapes.js");
 
 inquirer
-.prompt([
-  {
-    type: "input",
-    message: "What would is the name of the logo?/n",
-    name: "title",
-  },
-  {
-    type: "input",
-    message: "What 3 characters would you like to use for your SVG?/n",
-    validate: (input) => {
-      if (input.length > 0 && input.length <= 3) {
-        return true;
-      } else {
-        return Promise.reject("Invalid Input");
-      }
+  .prompt([
+    {
+      type: "input",
+      message: "What is the name of the logo?\n",
+      name: "title",
     },
-    name: "text",
-  },
-  {
-    type: "input",
-    message: "what color would you like your text?/n",
-    validate: (input) => {
-      if (input.length > 0) {
-        return true;
-      } else {
-        return Promise.reject("Invalid Input");
-      }
+    {
+      type: "input",
+      message: "What 3 characters would you like to use for your SVG?\n",
+      validate: (input) => {
+        if (input.length > 0 && input.length <= 3) {
+          return true;
+        } else {
+          return Promise.reject("Invalid Input");
+        }
+      },
+      name: "text",
     },
-    name: "textColor",
-  },
-  {
-    type: "list",
-    message: "what shape would you prefer?/n",
-    choices: ["Triangle", "Circle", "Square"],
-    name: "logoShape",
-  },
-  {
-    type: "input",
-    message: "what color would you like your logo?/n",
-    validate: (input) => {
-      if (input.length > 0) {
-        return true;
-      } else {
-        return Promise.reject("Invalid Input");
-      }
+    {
+      type: "input",
+      message: "What color would you like your text?\n",
+      validate: (input) => {
+        if (input.length > 0) {
+          return true;
+        } else {
+          return Promise.reject("Invalid Input");
+        }
+      },
+      name: "textColor",
     },
-    name: "logoColor",
-  },
-])
-  .then(data => {
-   const shapeClasses = {
-    Triangle: Triangle,
-    Circle: Circle,
-    Square: Square
-   };
+    {
+      type: "list",
+      message: "What shape would you prefer?\n",
+      choices: ["Triangle", "Circle", "Square"],
+      name: "logoShape",
+    },
+    {
+      type: "input",
+      message: "What color would you like your logo?\n",
+      validate: (input) => {
+        if (input.length > 0) {
+          return true;
+        } else {
+          return Promise.reject("Invalid Input");
+        }
+      },
+      name: "logoColor",
+    },
+  ])
+  .then((data) => {
+    const shapeClasses = {
+      Triangle: Triangle,
+      Circle: Circle,
+      Square: Square,
+    };
 
-   const ShapeClass = shapeClasses[data.logoShape];
-  if (ShapeClass) {
-    const shape = new ShapeClass(data.text, data.color, data.textcolor);
-    const svgContent = shape.renderShape();
-    fs.writeFile(`examples/logo.svg`, svgContent, (err) => {
-      if (err) {
-        console.error("An error has occurred:", err);
-      } else {
-        console.info("Generated logo.svg!");
-      }
-    });
-  } else {
-    console.error("Invalid shape selected.");
-  }
-})
-
-
-
-
-
+    const ShapeClass = shapeClasses[data.logoShape];
+    if (ShapeClass) {
+      const shape = new ShapeClass(
+        data.text,
+        data.logoColor, // Update to data.logoColor
+        data.textColor // Update to data.textColor
+      );
+      const svgContent = shape.renderShape();
+      fs.writeFile(`examples/logo.svg`, svgContent, (err) => {
+        if (err) {
+          console.error("An error has occurred:", err);
+        } else {
+          console.info("Generated logo.svg!");
+        }
+      });
+    } else {
+      console.error("Invalid shape selected.");
+    }
+  });
